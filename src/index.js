c3476e3b77da857bb1d6c3c5ec32db18b3694564
Omar Ahmad <superomar121@gmail.com>
2022-05-31 17:26:16 +0400

    fixed: modularization of location-search

--- src/index.js
+++ src/index.js
@@ -1,48 +1,9 @@
 import './reset.css';
 import './style.css';
 // import sampleWeatherData from './sample-current-weather.json';
-import {
-  getCurrentWeatherData,
-  getRequiredWeatherData,
-} from './weather-script';
-import createSearchboxElement from './location-search';
+
+import LocationSearch from './location-search';
 
 const welcomeContainer = document.querySelector('#welcome');
-const locationSearchContainer = createSearchboxElement();
+const locationSearchContainer = LocationSearch().createSearchboxElement();
 welcomeContainer.appendChild(locationSearchContainer);
-
-const button = document.querySelector('#welcome > #location-search button');
-const formInput = document.querySelector('#welcome #location');
-
-formInput.addEventListener('input', () => {
-  if (formInput.validity.valid) {
-    button.disabled = false;
-    formInput.classList.remove('invalid');
-  } else {
-    button.disabled = true;
-    if (formInput.validity.valueMissing === false) {
-      formInput.classList.add('invalid');
-    } else {
-      formInput.classList.remove('invalid');
-    }
-  }
-});
-button.addEventListener('click', async () => {
-  // Fetch weather data from OpenWeather
-  console.log(formInput);
-  const weatherData = await getCurrentWeatherData(formInput.value);
-  // const weatherData = sampleWeatherData;
-  console.log(weatherData);
-  if (weatherData.cod === '400') {
-    alert('Empty');
-  } else if (weatherData.cod === '404') {
-    alert('Location not found.');
-  } else {
-    // Get needed fields only
-    const neededWeatherData = getRequiredWeatherData(weatherData);
-    console.log(neededWeatherData);
-
-    // Remove welcome screen
-    document.querySelector('#welcome').remove();
-  }
-});
--- src/location-search.js
+++ src/location-search.js
@@ -1,39 +1,98 @@
-// This module is responsible for the DOM
+// This module is responsible for the logic
 // of the location search box that allows
 // the user to submit a request to the API
+import {
+  getCurrentWeatherData,
+  getRequiredWeatherData,
+} from './weather-script';
 
-// Creates and returns a container element
-// of the weather location search box
-const createSearchboxElement = () => {
-  const container = document.createElement('div');
-  container.id = 'location-search';
-  //   Search Box Label
-  const label = document.createElement('label');
-  label.textContent = 'Location';
-  label.for = 'location';
-  // Search Box Input Element
-  const input = document.createElement('input');
-  input.type = 'text';
-  input.name = 'location';
-  input.id = 'location';
-  input.pattern = '^[A-Za-z -]+$';
-  input.placeholder = 'Enter a Location';
-  input.required = true;
-  console.log(input);
-  //   Span for error
-  const span = document.createElement('span');
-  span.classList.add('error-msg');
-  // Submit Button
-  const button = document.createElement('button');
-  button.textContent = 'Search';
-  button.disabled = true; // Initially disabled
-  // Appending to container
-  container.appendChild(label);
-  container.appendChild(input);
-  container.appendChild(span);
-  container.appendChild(button);
+// Object storing DOM elements of location
+const LocationSearch = () => {
+  let container;
+  let input;
+  let label;
+  let button;
+  // Event for verifying input value
+  const verifySearchBoxOnInput = () => {
+    // Get DOM elements
+    if (input.validity.valid) {
+      button.disabled = false;
+      input.classList.remove('invalid');
+    } else {
+      button.disabled = true;
+      if (input.validity.valueMissing === false) {
+        input.classList.add('invalid');
+      } else {
+        input.classList.remove('invalid');
+      }
+    }
+  };
 
-  return container;
+  // Event for clicking on Search button
+  const searchButtonEvent = async () => {
+    // Get DOM Element
+    // Fetch weather data from OpenWeather
+    console.log(input);
+    const weatherData = await getCurrentWeatherData(input.value);
+    // const weatherData = sampleWeatherData;
+    console.log(weatherData);
+    if (weatherData.cod === '400') {
+      alert('Empty');
+    } else if (weatherData.cod === '404') {
+      alert('Location not found.');
+    } else {
+      // Get needed fields only
+      const neededWeatherData = getRequiredWeatherData(weatherData);
+      console.log(neededWeatherData);
+
+      // Remove welcome screen
+      document.querySelector('#welcome').remove();
+    }
+  };
+
+  // Creates and returns a container element
+  // of the weather location search box
+  const createSearchboxElement = () => {
+    const containerDOM = document.createElement('div');
+    containerDOM.id = 'location-search';
+    //   Search Box Label
+    const labelDOM = document.createElement('label');
+    labelDOM.textContent = 'Location';
+    labelDOM.for = 'location';
+    // Search Box Input Element
+    const inputDOM = document.createElement('input');
+    inputDOM.type = 'text';
+    inputDOM.name = 'location';
+    inputDOM.id = 'location';
+    inputDOM.pattern = '^[A-Za-z -]+$';
+    inputDOM.placeholder = 'Enter a Location';
+    inputDOM.required = true;
+    inputDOM.addEventListener('input', verifySearchBoxOnInput); // Search box event
+    //   Span for error messages
+    const span = document.createElement('span');
+    span.classList.add('error-msg');
+    // Submit Button
+    const buttonDOM = document.createElement('button');
+    buttonDOM.textContent = 'Search';
+    buttonDOM.disabled = true; // Initially disabled
+    buttonDOM.addEventListener('click', searchButtonEvent); // Search button event
+    // Appending to container
+    containerDOM.appendChild(labelDOM);
+    containerDOM.appendChild(inputDOM);
+    containerDOM.appendChild(span);
+    containerDOM.appendChild(buttonDOM);
+
+    container = containerDOM;
+    label = labelDOM;
+    input = inputDOM;
+    button = buttonDOM;
+
+    return container;
+  };
+
+  return {
+    createSearchboxElement,
+  };
 };
 
-export default createSearchboxElement;
+export default LocationSearch;
