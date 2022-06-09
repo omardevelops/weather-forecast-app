dc67743277cab0be462fd367eb02d35928528fe1
Omar Ahmad <superomar121@gmail.com>
2022-06-09 18:31:45 +0400

    clean up index.js

--- src/index.js
+++ src/index.js
@@ -2,35 +2,7 @@
 import './reset.css';
 import './style.css';
 // ### Import JS Modules ###
-import {
-  searchLocation,
-  getFiveDayWeatherData,
-  aggregateWeatherData,
-  getCurrentWeather,
-} from './api-functions';
 
-import sampleWeather from './sample-data.json';
-import {
-  updateDailyView,
-  updateCurrentWeatherView,
-  updateHourlyView,
-  loadingView,
-  initializeEventListeners,
-} from './dom-ui';
-
-import sampleCurrentWeather from './sample-current-weather.json';
+import { initializeEventListeners } from './dom-ui';
 
 initializeEventListeners();
-
-// Code segment for searching geolocation
-// let timer = null;
-// searchbox.addEventListener('keyup', (key) => {
-//   if (key.code !== 'Enter' && searchbox.value !== '') {
-//     clearTimeout(timer);
-//     timer = setTimeout(() => searchLocation(searchbox.value), 500);
-//   }
-// });
-
-// console.log(getFiveDayWeatherData(40.7127281, -74.0060152));
-// console.log(sampleWeather);
-console.log(aggregateWeatherData(sampleWeather));
